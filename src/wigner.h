5b326258c5901a7711810c8411f1a32d3a49eebd
Nicolas Tessore <n.tessore@ucl.ac.uk>
2020-11-09 09:33:02 +0000

    wigner_3j_m: add function to header

--- src/wigner.h
+++ src/wigner.h
@@ -6,3 +6,6 @@ void wigner_d_l(int lmin, int lmax, int m1, int m2, double theta, double* d);
 
 int wigner_3j_l(double l2, double l3, double m2, double m3, double* l1min,
                 double* l1max, double* thrcof, int ndim);
+
+int wigner_3j_m(double l1, double l2, double l3, double m1, double* m2min,
+                double* m2max, double* thrcof, int ndim);
